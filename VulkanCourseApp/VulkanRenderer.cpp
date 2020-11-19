d66c694d919fe7017e1e6fd0986f1f4844ada8ad
Greg Vardy <vardygreg23@gmail.com>
2020-11-19 20:55:15 +0000

    initial commit

---
+++ VulkanCourseApp/VulkanRenderer.cpp
@@ -0,0 +1,36 @@
+#include "VulkanRenderer.h"
+
+int VulkanRenderer::init(GLFWwindow* newWindow) 
+{
+    
+    window = newWindow;
+
+    try 
+    {
+        CreateInstance();
+    }
+    catch (const std::runtime_error &e)
+    {
+        printf("ERROR: %s \n", e.what());
+        return EXIT_FAILURE;
+    }
+
+    return 0;
+}
+
+void VulkanRenderer::CreateInstance() 
+{
+    // Information about the application itself
+    // Most data here doesn't affect the program and is for dev convenience.
+    VkApplicationInfo       appInfo                     = {};
+                            appInfo.sType               = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+                            appInfo.pApplicationName    = "Vulkan App";
+                            appInfo.applicationVersion  = VK_MAKE_VERSION(1,0,0);
+                            appInfo.pEngineName         = "Noe Engine";
+                            appInfo.engineVersion       = VK_MAKE_VERSION(1,0,0);
+                            appInfo.apiVersion          = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo    createInfo                  = {};
+                            createInfo.sType            = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+                            createInfo.pApplicationInfo = &appInfo;
+}
---
+++ VulkanCourseApp/main.cpp
@@ -0,0 +1,39 @@
+#define GLFW_INCLUDE_VULKAN
+#include    "GLFW/glfw3.h"
+#include    "VulkanRenderer.h"
+#include    <iostream>
+#include    <stdexcept>
+#include    <vector>
+
+GLFWwindow* window;
+VulkanRenderer vulkanRenderer;
+
+void initWindow(std::string wName = "Test Window", 
+                const int width = 800, 
+                const int height = 600)
+{
+    // Initialize glfw
+    glfwInit();
+
+    // Set GLFW to not work with OpenGL
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    window = glfwCreateWindow(width, height, wName.c_str(),nullptr,nullptr);
+}
+
+int main() 
+{
+    initWindow();
+
+    if(vulkanRenderer.init(window) == EXIT_FAILURE) return EXIT_FAILURE;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
